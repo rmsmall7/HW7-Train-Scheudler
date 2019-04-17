526452f231c803f0c9e1f84f51051f1e9220a931
unknown <rmsmall7@gmail.com>
2019-04-17 12:10:13 -0400

    updated js- final version

--- javascript/app.js
+++ javascript/app.js
@@ -8,7 +8,7 @@
 
 
 
-      // Initialize Firebase
+      // Initialize Firebase -  copied this from firebase after creating the db
       var config = {
         apiKey: "AIzaSyDS4BM4oQwdfiRwnQ4qf_BwYfqbrbK8DiI",
         authDomain: "train-scheduler-efa77.firebaseapp.com",
@@ -22,19 +22,19 @@
       //this will give access to the db
       var trainData = firebase.database();
 
-      //setting the var for the current time in the header of the html
+      //setting the var for the current time in the header of the html -  using moment.js for this
       setInterval(function (startTime) {
         $("#timer").html(moment().format('hh:mm a'))
       }, 1000);
 
-      //get info from the form / make an object to put the info
+      //get info from the form / make an object to put the info -  The vars I created are getting the input from the form on the html after the user selects the submit button
       $("#addTrainBtn").on("click", function() {
         var trainName = $("#trainNameInput").val().trim();
         var destination = $("#destinationInput").val().trim();
         var firstTrain = moment($("#firstTrainInput").val().trim(), "HH:mm").subtract(10, "years").format("X");
         var frequency = $("#frequencyInput").val().trim();
 
-        //console.log to make sure the train info is submitting when submit is selected
+        //console.log to make sure the train info is submitting when submit is selected -  commented this out after checking to make sure it was working.  Keeping it as I can use it later if debugging is needed
         // console.log(trainName);
         // console.log(destination);
         // console.log(firstTrain);
@@ -49,7 +49,7 @@
           frequency: frequency
         }
 
-        //code to push the user entered data to firebase
+        //code to push the user entered data to firebase and will store it in the newTrain format for use later
         trainData.ref().push(newTrain);
 
         //simple alert for the user to know that they did add a train successfully
@@ -63,19 +63,20 @@
 
         return false;        
       })   
-
+    
+      //this is getting a snapshot of the information the user has added.  Will append this information later in the html
       trainData.ref().on("child_added", function(snapshot) {
         var name= snapshot.val().name;
         var destination = snapshot.val().destination;
         var frequency = snapshot.val().frequency;
         var firstTrain = snapshot.val().firstTrain;
 
-        //using moment.js to figure out time until next train
+        //using moment.js to figure out time until next train--  creating new variables to calculate the time until next train
         var remainder = moment().diff(moment.unix(firstTrain),"minutes")%frequency;
         var minutes = frequency - remainder;
         var arrival = moment().add(minutes,"m").format("hh:mm A");
 
-        //console log to check to make sure these are calculating correctly
+        //console log to check to make sure these are calculating correctly.  Keeping this to use later for debugging if needed
         // console.log(remainder);
         // console.log(minutes);
         // console.log(arrival);
